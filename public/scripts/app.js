336916618359ee7fecf7c57eb499c1b4c374cc57
JillGlass <glass.nz@gmail.com>
2020-06-19 12:37:43 +1200

    template with : ? and &&

---
+++ public/scripts/app.js
@@ -0,0 +1,81 @@
+"use strict";
+
+console.log("app.js is running");
+
+var app = {
+  title: "Indecision App",
+  subTitle: "Put your life in the hands of a computer",
+  options: ['One', 'Two']
+};
+
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subTitle && React.createElement(
+    "p",
+    null,
+    app.subTitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
+);
+
+var user = {
+  name: "Jill",
+  age: 39,
+  location: "Auckland"
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      "Location: ",
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  getLocation(user.location)
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
